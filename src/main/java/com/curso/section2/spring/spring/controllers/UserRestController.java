ac34551af730f8e6f02da7c14cd69b319e83725f
DevilAbbath <79130973+DevilAbbath@users.noreply.github.com>
2024-06-10 00:37:52 -0400

    Aplicacion de DTO

--- src/main/java/com/curso/section2/spring/spring/controllers/UserRestController.java
+++ src/main/java/com/curso/section2/spring/spring/controllers/UserRestController.java
@@ -1,6 +1,9 @@
 package com.curso.section2.spring.spring.controllers;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,20 +11,51 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.curso.section2.spring.spring.models.User;
+import com.curso.section2.spring.spring.models.dto.UserDto;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 @RequestMapping("/api")
 public class UserRestController {
 
    @GetMapping("/details")
-   public Map<String, Object> details() {
+   public UserDto details() {
       User user = new User("Erick", "Jaime");
-      Map<String, Object> body = new HashMap<>();
+      UserDto userDto = new UserDto();
 
-      body.put("title", "Details");
-      body.put("user", user);
+      /*
+       * Without DTO
+       * Map<String, Object> body = new HashMap<>();
+       * 
+       * body.put("title", "Details");
+       * body.put("user", user);
+       */
 
-      return body;
+      /* With DTO */
+      userDto.setUser(user);
+      userDto.setTitle("Details");
+
+      return userDto;
+   }
+
+   @GetMapping("/list")
+   public List<User> list() {
+      User user = new User("Erick", "Jaime");
+      User user2 = new User("Esteban", "Jara");
+      User user3 = new User("Eduardo", "Jair");
+
+      /* Abreviation */
+      List<User> users = Arrays.asList(user, user2, user3);
+
+      /*
+       * Desgloce
+       * List<User> users = new ArrayList<>();
+       * users.add(user);
+       * users.add(user2);
+       * users.add(user3);
+       */
+
+      return users;
    }
 
 }
